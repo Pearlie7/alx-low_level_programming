4989ff75b89acf440b18535c20284f6c662de6ae
pearlie <dpearlannie@gmail.com>
2023-03-16 16:53:18 -0700

    A program to print a sentence without using puts or printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - main function
+*
+* Return: return 1 if code is successful
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n," 59);
+	return (1);
+}	
