d4a974ea99bdfaa227092d808fded31c87415e7d
pearlie <dpearlannie@gmail.com>
2023-03-21 08:28:36 -0700

    Task 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print lowercase alphabet 1    0 times
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+		while (i <= 0)
+		{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+		}
+}
