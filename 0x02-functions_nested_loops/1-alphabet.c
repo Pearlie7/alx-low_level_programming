f7a2d061383446ad6ecc205e3e6d649adc4a1220
pearlie <dpearlannie@gmail.com>
2023-03-21 07:26:03 -0700

    Task 1 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* 1-alphabet.c - prints alphabet in lowercase
+* Return: void
+*/
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+		_putchar('\n');
+}
