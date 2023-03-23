c8af317877f06f43dce1545307f165ccb3d669eb
pearlie <dpearlannie@gmail.com>
2023-03-23 04:38:35 -0700

    Task 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _isupper - prints the uppercase character
+*@c: the character to be checked
+* Return: 1 on success otherwise return 0
+*/
+int _isupper(int c)
+{
+	c = 'A';
+
+		if (c <= 'Z')
+		{
+			return (1);
+		}
+				return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Task1		-	 A function that checks for uppercase character
