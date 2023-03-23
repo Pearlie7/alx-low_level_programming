43a211f166a68c5b7de513d58c1384f07ac40c0e
pearlie <dpearlannie@gmail.com>
2023-03-23 10:20:56 -0700

    Task 0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - to test that the function positive_or_negative is successful
+* Return: 0
+*/
+
+int main(void)
+{
+
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+	return (0);
+}
