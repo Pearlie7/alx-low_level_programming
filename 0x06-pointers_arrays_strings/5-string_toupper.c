a2abdf0f2009fc57af0ecb7d5cee1479143a82c2
pearlie <dpearlannie@gmail.com>
2023-03-30 07:23:24 -0700

    Task 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,13 @@
 
 char *string_toupper(char *str)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index])
+	while (str[i] != '\0')
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-		{
-			str[index] -= 32;
-			index++;
-		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
 	}
 	return (str);
 }
