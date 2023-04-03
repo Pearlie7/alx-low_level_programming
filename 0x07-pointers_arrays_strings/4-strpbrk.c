339f0fd768d724976fe85884f55f22af5fcc5775
pearlie <dpearlannie@gmail.com>
2023-04-03 06:43:14 -0700

    Task 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (n = 0; s[n] != '\0'; n++)
+		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
 				return (s + i);
