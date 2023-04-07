01e393d7c2fb73d07748489c0f2cd01897a3edfc
pearlie <dpearlannie@gmail.com>
2023-04-07 02:27:06 -0700

    Task 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	for (argc = 1; argc >= 1; argc++)
+	{
+	printf("%d\n", argc);
+	}
+	return (0);
+}
