b15362da6709adc58dacbfec1c727b33b63a5887
pearlie <dpearlannie@gmail.com>
2023-04-07 03:28:49 -0700

    Task 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int j;
+	unsigned int sum = 0;
+	char *a;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			a = argv[i];
+			for (j = 0; j < strlen(a); j++)
+			{
+				if (a[j] < 48 || a[j] >57)
+				{
+					printf("error\n");
+					return (1);
+				}
+			}
+			sum += atoi(a);
+			a++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
