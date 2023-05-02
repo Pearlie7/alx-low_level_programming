bf1ac28f108a6097b57c06a3ae38fa22d173a31f
Pearlie7 <dpearlannie@gmail.com>
2023-05-02 05:15:09 -0700

    Task 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer to the first node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t nodenum = 0;
+
+	while (cursor != NULL)
+	{
+		printf("%d\n", cursor->n);
+		nodenum++;
+		cursor = cursor->next;
+	}
+
+	return (nodenum);
+}
