8211a01a9e4c5801375052cac0de787b4d03bed4
Pearlie7 <dpearlannie@gmail.com>
2023-06-19 11:44:13 -0700

    Task 100-operations.so

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,10 +1,70 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
-#include <stdlib.h>
 int mul(int a, int b);
 int add(int a, int b);
 int sub(int a, int b);
 int mol(int a, int b);
 int divide(int a, int b);
 
- #endif /* DYNAMIC_LIB */
+/**
+ * add - adds two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts a number from another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides one number by another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int divide(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder when a number is divided by another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int mol(int a, int b)
+{
+	return (a % b);
+}
