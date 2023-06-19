37d3de0f1267a67849678479662aa35ad43faeee
Pearlie7 <dpearlannie@gmail.com>
2023-06-19 11:14:52 -0700

    Task 100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,10 @@
+#ifndef DYNAMIC_LIB
+#define DYNAMIC_LIB
+#include <stdlib.h>
+int mul(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int mol(int a, int b);
+int divide(int a, int b);
+
+ #endif /* DYNAMIC_LIB */
