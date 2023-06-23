89d40b2f7febf8a859f3c32ee02132055daab6c7
Pearlie7 <dpearlannie@gmail.com>
2023-06-23 13:08:56 -0700

    Advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P tmp/ https://github.com/pearlie7/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/Pearlie7/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/jackpot.so
